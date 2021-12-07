9388ebec15f104e253d28cf76fddfcf48b2fccbe
Emin Hamzagic <hamzagicemin4@gmail.com>
2021-12-07 21:08:10 +0100

    work in progress

--- Dot Catcher/main.js
+++ Dot Catcher/main.js
@@ -1,6 +1,9 @@
 var playground = document.getElementById('playground');
 
-for(i = 0; i < 50; i++){
+var cols = 20;
+var rows = 10;
+
+for(i = 0; i < cols * rows; i++){
     playground.innerHTML += '<div class = "field "><div>\n';
 }
 
@@ -8,31 +11,39 @@ var fields = document.getElementsByClassName('field');
 
 fields[fields.length - 1].classList.add('player');
 
-var player_position = 49;
+var player_position = cols * rows - 1;
 
-fields_arr = Array.from(fields);
-
-// for (x of fields_arr){
-//     if (x.classList.contains('player')){
-//         console.log(fields_arr.indexOf(x))
-//         break
-//     }
-// }
-
+// fields_arr = Array.from(fields);
 
 function MoveUp(){
-    if (player_position > 9){
+    if (player_position > cols - 1){
         fields[player_position].classList.remove('player');
-        fields[player_position - 10].classList.add('player');
-        player_position -= 10;
+        fields[player_position - cols].classList.add('player');
+        player_position -= cols;
     }
 }
 
 function MoveDown(){
-    if (player_position < 40){
+    if (player_position < rows * cols - cols){
         fields[player_position].classList.remove('player');
-        fields[player_position + 10].classList.add('player');
-        player_position += 10;
+        fields[player_position + cols].classList.add('player');
+        player_position += cols;
+    }
+}
+
+function MoveLeft(){
+    if (player_position % cols > 0){
+        fields[player_position].classList.remove('player');
+        fields[player_position -1].classList.add('player');
+        player_position -= 1;
+    }
+}
+
+function MoveRight(){
+    if (player_position % cols < cols - 1){
+        fields[player_position].classList.remove('player');
+        fields[player_position +1].classList.add('player');
+        player_position += 1;
     }
 }
 
@@ -43,4 +54,10 @@ window.onkeydown = function(event) {
     else if (event.keyCode == 83){
         MoveDown();
     }
- }
+    else if(event.keyCode == 65){
+        MoveLeft();
+    }
+    else if(event.keyCode == 68){
+        MoveRight();
+    }
+}
